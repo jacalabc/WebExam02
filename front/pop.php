1fb7ae822963691362da4fd0782cb06f9c348070
jerry <jerry19971228@gmail.com>
2023-01-16 09:57:24 +0800

    使用css和js來製作彈出視窗效果

--- front/pop.php
+++ front/pop.php
@@ -1,46 +1,100 @@
+<style>
+    .full{
+        display: none;
+        position:absolute;
+        background-color: rgb(100,100,100);
+        z-index: 99;
+        padding:1rem;
+        box-shadow: 0 0 10px #999;
+        left:-10px;
+        top:5px;
+        width:95%;
+        height:500px;
+        overflow: auto;
+    }
+
+    .news-title{
+        cursor: pointer;
+        background-color: #eee;
+    }
+</style>
 <fieldset>
-    <legend>目前位置 : 首頁 > 人氣文章區</legend>
+    <legend>目前位置：首頁 > 人氣文章區</legend>
+
     <table>
         <tr>
             <td width="30%">標題</td>
             <td width="50%">內容</td>
             <td>人氣</td>
         </tr>
-        <?php
-        $all = $News->count(['sh' => 1]);
-        $div = 5;
-        $pages = ceil($all / $div);
-        $now = $_GET['p'] ?? 1;
-        $start = ($now - 1) * $div;
-
-        $rows = $News->all(['sh' => 1], " order by `good` desc limit $start,$div");
-        foreach ($rows as $row) {
-
-        ?>
-            <tr>
-                <td><?= $row['title']; ?></td>
-                <td><?= mb_substr($row['text'], 0, 20); ?>...</td>
-                <td></td>
-            </tr>
-        <?php
-
-        }
-        ?>
-    </table>
     <?php
-    if (($now - 1) > 0) {
-        $prev = $now - 1;
-        echo "<a href='index.php?do=pop&p=$prev'> < </a>";
-    }
+    $all=$News->count(['sh'=>1]);
+    $div=5;
+    $pages=ceil($all/$div);
+    $now=$_GET['p']??1;
+    $start=($now-1)*$div;
 
-    for ($i = 1; $i <= $pages; $i++) {
-        $size = ($i == $now) ? "26px" : "16px";
-        echo "<a href='index.php?do=pop&p=$i' style='font-size:$size'> $i </a>";
-    }
+    $rows=$News->all(['sh'=>1]," order by `good` desc limit $start,$div");
+    foreach($rows as $row){
 
-    if (($now + 1) <= $pages) {
-        $next = $now + 1;
-        echo "<a href='index.php?do=pop&p=$next'> > </a>";
-    }
     ?>
+        <tr>
+        <td class='news-title'><?=$row['title'];?></td>
+            <td style='position:relative'>
+                <div class="short"><?=mb_substr($row['text'],0,20);?>...</div>
+                <div class="full">
+                    <?php
+                    echo "<div style='color:skyblue'>".$row['type']."</div>";
+                    echo "<div style='color:white'>".nl2br($row['text'])."</div>";
+                    ?>
+                </div>
+            </td>
+            <td></td>
+        </tr>
+    <?php
+    }
+    ?>        
+    </table>
+    <div>
+     <?php
+     if(($now-1)>0){
+        $prev=$now-1;
+        echo "<a href='index.php?do=pop&p=$prev'> < </a>";
+     }
+
+     for($i=1;$i<=$pages;$i++){
+        $size=($now==$i)?"26px":"16px";
+        echo "<a href='index.php?do=pop&p=$i' style='font-size:$size'> $i </a>";
+     }
+
+     if(($now+1)<=$pages){
+        $next=$now+1;
+        echo "<a href='index.php?do=pop&p=$next'> > </a>";
+     }
+     
+     ?>
+    </div>
+
 </fieldset>
+<script>
+    $(".news-title").hover(
+        function(){
+        
+            $(this).next().children('.full').show()
+        
+        },
+
+        function(){
+            $(this).next().children('.full').hide()
+        }
+    )
+    $(".full").hover(
+        function(){
+            $(this).show();
+        },
+        function(){
+            $(this).hide();
+        }
+
+    )
+</script>
